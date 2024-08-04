125ff7b6e35270befc689313ea1826c1c55227eb
Jonathon Jones <jjones.det@gmail.com>
2024-08-03 21:12:43 -0500

    Display extended forecast

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -35,7 +35,7 @@ class ForecastsController < ApplicationController
   helper_method :weather
 
   def fetch_weather
-    uri = URI.parse("https://api.open-meteo.com/v1/forecast?latitude=#{latitude}&longitude=#{longitude}&current=temperature_2m&format=json&temperature_unit=fahrenheit")
+    uri = URI.parse("https://api.open-meteo.com/v1/forecast?latitude=#{latitude}&longitude=#{longitude}&current=temperature_2m&format=json&temperature_unit=fahrenheit&daily=temperature_2m_max,temperature_2m_min")
     JSON.parse(Net::HTTP.get(uri, {Referer: 'Forecasts Toy Project'}))
   end
 
@@ -43,4 +43,13 @@ class ForecastsController < ApplicationController
     weather['current']['temperature_2m']
   end
   helper_method :current_temperature
+
+  # Data comes in as "daily"=>{"time"=>["2024-08-04", "2024-08-05", "2024-08-06", "2024-08-07", "2024-08-08", "2024-08-09", "2024-08-10"], "temperature_2m_max"=>[91.1, 91.8, 90.6, 83.1, 87.0, 85.3, 81.9], "temperature_2m_min"=>[70.0, 70.3, 70.3, 70.8, 66.3, 67.3, 62.3]}}
+  def extended_forecast
+    daily = weather['daily']
+    daily['time'].map.with_index do |date, index|
+      {date: date, high: daily['temperature_2m_max'][index], low: daily['temperature_2m_min'][index]}
+    end
+  end
+  helper_method :extended_forecast
 end
