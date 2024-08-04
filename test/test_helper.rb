c2c5028844646c78be9c0c2183d7660579423369
Jonathon Jones <jjones.det@gmail.com>
2024-08-03 22:51:25 -0500

    Remove extra references to database

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,9 +9,6 @@ module ActiveSupport
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-
     VCR.configure do |c|
       c.cassette_library_dir = "test/vcr_cassettes"
       c.hook_into :webmock
