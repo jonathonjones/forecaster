6ebee54cb673524362ad93edfa3869080118cfb0
Jonathon Jones <jjones.det@gmail.com>
2024-08-03 20:41:01 -0500

    Display current temperature

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -12,10 +12,10 @@ class ForecastsController < ApplicationController
   helper_method :address
 
   def geocode_data
-    @geocode_data ||= fetch_geocode_data!
+    @geocode_data ||= fetch_geocode_data
   end
 
-  def fetch_geocode_data!
+  def fetch_geocode_data
     address_component = URI.encode_www_form_component(address)
     uri = URI.parse("https://nominatim.openstreetmap.org/search.php?q=#{address_component}&format=jsonv2")
     JSON.parse(Net::HTTP.get(uri, {Referer: 'Forecasts Toy Project'}))
@@ -24,16 +24,23 @@ class ForecastsController < ApplicationController
   def latitude
     geocode_data.first['lat']
   end
-  helper_method :latitude
 
   def longitude
     geocode_data.first['lon']
   end
-  helper_method :longitude
 
   def weather
+    @weather ||= fetch_weather
+  end
+  helper_method :weather
+
+  def fetch_weather
     uri = URI.parse("https://api.open-meteo.com/v1/forecast?latitude=#{latitude}&longitude=#{longitude}&current=temperature_2m&format=json&temperature_unit=fahrenheit")
     JSON.parse(Net::HTTP.get(uri, {Referer: 'Forecasts Toy Project'}))
   end
-  helper_method :weather
+
+  def current_temperature
+    weather['current']['temperature_2m']
+  end
+  helper_method :current_temperature
 end
